a628d419f426fdb9087e34faaca178d16c59c267
Aylhenia <lcastillaiamoros@gmail.com>
2023-07-11 16:41:15 +0200

    Add JS for the onscreen cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,32 @@
-
+function updateTime() {
+  //Los Angeles
+  let losAngeles = document.querySelector("#LA");
+  let losAngelesDate = losAngeles.querySelector(".date");
+  let losAngelesTime = losAngeles.querySelector(".time");
+  losAngelesTime.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
+  losAngelesDate.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("MMMM Do YYYY");
+  //Barcelona
+  let barcelona = document.querySelector("#bcn");
+  let barcelonaDate = barcelona.querySelector(".date");
+  let barcelonaTime = barcelona.querySelector(".time");
+  barcelonaTime.innerHTML = moment()
+    .tz("Europe/Andorra")
+    .format("h:mm:ss [<small>]A[</small>]");
+  barcelonaDate.innerHTML = moment()
+    .tz("Europe/Andorra")
+    .format("MMMM Do YYYY");
+  //Tokyo
+  let tokyo = document.querySelector("#tokyo");
+  let tokyoDate = tokyo.querySelector(".date");
+  let tokyoTime = tokyo.querySelector(".time");
+  tokyoTime.innerHTML = moment()
+    .tz("Asia/Tokyo")
+    .format("h:mm:ss [<small>]A[</small>]");
+  tokyoDate.innerHTML = moment().tz("Asia/Tokyo").format("MMMM Do YYYY");
+}
+updateTime();
+setInterval(updateTime, 1000);
